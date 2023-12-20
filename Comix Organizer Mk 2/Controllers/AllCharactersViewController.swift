f8688181ab54e4ecad9848850c138a0942bc424c
Noah Pope <npope@tutanota.com>
2023-12-20 09:22:22 -0500

    providing data for AllCharactersVC, but having touble filtering

--- Comix Organizer Mk 2/Controllers/AllCharactersViewController.swift
+++ Comix Organizer Mk 2/Controllers/AllCharactersViewController.swift
@@ -22,9 +22,27 @@ class AllCharactersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
         view.backgroundColor = .systemBackground
         title = "Characters"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+//        Task {
+//            await configureCharacters(with: )
+//            tableView.delegate = self
+//            tableView.dataSource = self
+//            tableView.frame = view.bounds
+//        }
     }
+    
+    private func configureCharacters(with publisher: String) async {
+        if let results = try? await APICaller.shared.getCharacters() {
+            self.characters += results.filter {$0.publisher == publisher}
+        } else {
+            print("something went wrong in configureCharacters()")
+        }
+        
+    }
+
 }
