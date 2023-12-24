759097094a778fd41d98b3ca0682c3efa81d6cf1
Noah Pope <npope@tutanota.com>
2023-12-24 10:27:46 -0500

    changing character model to account for nested dictionary

--- Comix Organizer Mk 2/Models/Character.swift
+++ Comix Organizer Mk 2/Models/Character.swift
@@ -7,13 +7,27 @@
 
 import Foundation
 
-struct APICharactersResponse: Codable {
+struct APICharactersResponse: Decodable {
     let results: [Character]
 }
 
-struct Character: Codable {
+
+//changing to just Decodable
+//Codable protocol throws error after new init
+struct Character: Decodable {
+       
     let id: Int
-    let name: String
-    let publisher: [String: Any]
-    //how to get image in here? UIImage throws error for codable protocol
+    let characterName: String
+    let publisher: Dictionary<String, Any>
+    
+    enum CodingKeys: String, CodingKey {
+        case id, publisher
+        case characterName = "name"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+//        let dictionary: [String: Any] = try container.decode([String: Any].self, forKey: key)
+    }
 }
