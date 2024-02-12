ab728175e8b9e40f5f5372c4c2fbbefba48baee8
Noah Pope <npope@tutanota.com>
2024-02-12 08:56:13 -0500

    adding shared selected publisher model to MainTBC & setting up title and character search VCs

---
+++ Comix Organizer Mk 2/Controllers/View Controllers/CharacterSearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterSearchViewController.swift
+//  Comix Organizer Mk 2
+//
+//  Created by Noah Pope on 2/12/24.
+//
+
+import Foundation
---
+++ Comix Organizer Mk 2/Controllers/View Controllers/TitleSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  Comix Organizer Mk 2
+//
+//  Created by Noah Pope on 2/12/24.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
