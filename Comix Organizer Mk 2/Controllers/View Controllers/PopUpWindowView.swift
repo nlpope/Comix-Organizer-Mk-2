4541dd93f2dc3525a81653c88a7f70341b932379
Noah Pope <npope@tutanota.com>
2024-02-20 08:07:30 -0500

    adding popupwindowView to hold popupwindowVC (to be created)

---
+++ Comix Organizer Mk 2/Controllers/View Controllers/PopUpWindowView.swift
@@ -0,0 +1,23 @@
+//
+//  PopUpSelectorView.swift
+//  Comix Organizer Mk 2
+//
+//  Created by Noah Pope on 2/19/24.
+//
+
+import Foundation
+import UIKit
+
+private class PopUpWindowView: UIView {
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+    
--- Comix Organizer Mk 2/Controllers/View Controllers/TitlesCharactersPopUpViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TitlesCharactersPopUpViewController.swift
-//  Comix Organizer Mk 2
-//
-//  Created by Noah Pope on 2/19/24.
-//
-
-import Foundation
