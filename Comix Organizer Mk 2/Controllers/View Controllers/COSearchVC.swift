aacd3bfc0b1e62e8f97a3b3c02f1315bf9938246
Noah Pope <npope@tutanota.com>
2024-06-28 14:17:56 -0500

    fiixed loading animation dots by setting 'animation' prop to true

---
+++ Comix Organizer Mk 2/Controllers/View Controllers/COSearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  COStartVC.swift
+//  Comix Organizer Mk 2
+//
+//  Created by Noah Pope on 6/28/24.
+//
+
+import UIKit
+
+class COSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
