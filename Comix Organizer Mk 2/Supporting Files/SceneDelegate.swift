f82b9ef11ef0b0ee4cc639ebee15778549b0a98c
Noah Pope <npope@tutanota.com>
2024-11-19 20:29:21 -0600

    removing print statements from scenedelegate

--- Comix Organizer Mk 2/Supporting Files/SceneDelegate.swift
+++ Comix Organizer Mk 2/Supporting Files/SceneDelegate.swift
@@ -31,13 +31,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        print("didbecomeactive")
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        print("resignactive")
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
@@ -48,7 +46,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        print("enteredbackground")
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
